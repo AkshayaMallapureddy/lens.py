0f53b1f90b13f20e411153ea2492aa4fb0272b13
AkshayaMallapureddy <akshayamallapureddy421@gmail.com>
2025-03-06 10:00:42 +0530

    Add files via upload

---
+++ lens.py
@@ -0,0 +1,24 @@
+import google.generativeai as genai
+from PIL import Image
+import streamlit as st
+
+genai.configure(api_key="AIzaSyCIk4YLufVS_Kp2ldrKHEyAgtSgCsotY8Q")
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+def get_response(image,prompt):
+    response = model.generate_content([image,prompt])
+    return response.text
+
+
+#streamlit
+st.title("lens")
+image = st.file_uploader("Upload the image",type=["jpeg","png"])
+input = st.text_input("enter the question for image")
+button = st.button("click here")
+#display
+if button:
+    im = Image.open(image)
+    st.image(im)
+    output = get_response(im,input)
+    st.write(output)
